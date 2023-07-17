0cac4182acfc236c93b4cc51e784c132bacaaf42
YunSeunghwan <dksrud123456@gmail.com>
2023-07-17 15:50:43 +0900

    additional commit

--- calculator.js
+++ calculator.js
@@ -1,13 +1,63 @@
+let temp = 0.0;
+let currentOperator = null;
+let operatorAppended = false;
+
 function appendToResult(value) {
-    document.getElementById('result').value += value;
+    resultText = document.getElementById('result').value
+    if (false){}
+    else if (operatorAppended && value === '.'){
+      document.getElementById('result').value = '0.';  
+      operatorAppended = false;
+    }
+    else if (resultText.includes('.') && value === '.'){
+    } 
+    else if (operatorAppended){
+      document.getElementById('result').value = value;  
+      operatorAppended = false;
+    }
+    else if (resultText == '0'){
+      document.getElementById('result').value = value;
+    }
+    else {
+      document.getElementById('result').value += value;
+    }
 }
 
 function calculateResult() {
-    const result = document.getElementById('result').value;
-    const calculatedResult = eval(result);
-    document.getElementById('result').value = calculatedResult;
+  if (currentOperator =='+'){
+    temp = parseFloat(temp) + parseFloat(document.getElementById('result').value);
+  }
+  else if (currentOperator =='-'){
+    temp = parseFloat(temp) - parseFloat(document.getElementById('result').value);
+  }
+  else if (currentOperator =='*'){
+    temp = parseFloat(temp) * parseFloat(document.getElementById('result').value);
+  }
+  else if (currentOperator =='/'){
+    temp = parseFloat(temp) / parseFloat(document.getElementById('result').value);
+  }
+  else if (currentOperator ==null){
+    temp = document.getElementById('result').value;
+  }
+  else {
+    alert("invalid Operator!!");
+    return;
+  }
+  document.getElementById('result').value =parseFloat(parseFloat(temp).toFixed(10));
+
+  currentOperator = null;
+  operatorAppended = false;
 }
 
 function clearResult() {
-    document.getElementById('result').value = '';
+    document.getElementById('result').value = '0';
+    temp = 0.0
+    currentOperator = null;
+    operatorAppended = false;
+}
+
+function appendOperator(op){
+  calculateResult()
+  currentOperator = op;
+  operatorAppended = true;
 }
