0aa66f365c5195b61a99ebd476f4ac381d0ab3e8
YunSeunghwan <dksrud123456@gmail.com>
2023-07-23 18:57:36 +0900

    showResult applied

--- calculator.js
+++ calculator.js
@@ -1,52 +1,58 @@
 let temp = 0.0;
 let currentOperator = null;
-let operatorAppended = false;
+
+function showResult(value) {
+  document.getElementById('result').value = value;
+}
+
+function clearResult() {
+  showResult('0');
+  temp = 0.0;
+  currentOperator = null;
+}
 
 function appendToResult(value) {
-  let resultText;
-  resultText = document.getElementById('result').value;
-  if (operatorAppended && value === '.') {
-    document.getElementById('result').value = '0.';
-    operatorAppended = false;
+  let resultText = document.getElementById('result').value;
+  if (currentOperator != null && value === '.') {
+    // when [operator]-> [.] clicked
+    showResult('0.');
   } else if (resultText.includes('.') && value === '.') {
-    // not allowing two floatin-point
-  } else if (operatorAppended) {
-    document.getElementById('result').value = value;
-    operatorAppended = false;
+    // not allowing double floatin-point
+  } else if (currentOperator != null) {
+    // when [operator] -> [0-9] clicked
+    showResult(value);
   } else if (resultText === '0') {
-    document.getElementById('result').value = value;
+    // when [operator] clicked displaying 0
+    showResult(value);
   } else {
-    document.getElementById('result').value += value;
+    // when [0-9] clicked
+    resultText += value;
+    showResult(resultText);
   }
 }
 
 function calculateResult() {
+  const currentTemp = parseFloat(temp);
+  const currentResult = parseFloat(document.getElementById('result').value);
+
   if (currentOperator === '+') {
-    temp = parseFloat(temp) + parseFloat(document.getElementById('result').value);
+    temp = currentTemp + currentResult;
   } else if (currentOperator === '-') {
-    temp = parseFloat(temp) - parseFloat(document.getElementById('result').value);
+    temp = currentTemp - currentResult;
   } else if (currentOperator === '*') {
-    temp = parseFloat(temp) * parseFloat(document.getElementById('result').value);
+    temp = currentTemp * currentResult;
   } else if (currentOperator === '/') {
-    temp = parseFloat(temp) / parseFloat(document.getElementById('result').value);
+    temp = currentTemp / currentResult;
   } else if (currentOperator === null) {
     temp = document.getElementById('result').value;
   }
-  document.getElementById('result').value = parseFloat(parseFloat(temp).toFixed(10));
 
+  const result = parseFloat(parseFloat(temp).toFixed(10));
+  showResult(result);
   currentOperator = null;
-  operatorAppended = false;
-}
-
-function clearResult() {
-  document.getElementById('result').value = '0';
-  temp = 0.0;
-  currentOperator = null;
-  operatorAppended = false;
 }
 
 function appendOperator(op) {
   calculateResult();
   currentOperator = op;
-  operatorAppended = true;
 }
