df523bddb6147a8de90e002a243b3e66b10aa971
YunSeunghwan <dksrud123456@gmail.com>
2023-07-23 20:41:25 +0900

    readable code

--- calculator.js
+++ calculator.js
@@ -1,5 +1,6 @@
 let temp = 0.0;
 let currentOperator = null;
+let operatorAppended = false;
 
 function showResult(value) {
   document.getElementById('result').value = value;
@@ -12,39 +13,70 @@ function clearResult() {
 }
 
 function appendToResult(value) {
-  let resultText = document.getElementById('result').value;
-  if (currentOperator != null && value === '.') {
-    // when [operator]-> [.] clicked
-    showResult('0.');
-  } else if (resultText.includes('.') && value === '.') {
-    // not allowing double floatin-point
-  } else if (currentOperator != null) {
-    // when [operator] -> [0-9] clicked
-    showResult(value);
-  } else if (resultText === '0') {
-    // when [operator] clicked displaying 0
-    showResult(value);
+  if (operatorAppended) {
+    if (value === '.') {
+      // when [operator]-> [.] clicked
+      showResult('0.');
+    } else {
+      showResult(value);
+    }
+    operatorAppended = false;
   } else {
-    // when [0-9] clicked
-    resultText += value;
-    showResult(resultText);
+    let resultText = document.getElementById('result').value;
+    switch(value) {
+      case '.':
+        if (resultText.includes('.')) {
+          // pass. (double floatin-point)
+        } else if(resultText === '0') {
+          showResult('0.')
+        } else {
+          resultText += value;
+          showResult(resultText);
+        }
+        break;
+        case '0':
+          if (resultText === '0') {
+            // pass. (double zero)
+          } else {
+          resultText += value;
+          showResult(resultText);
+        }
+        break;
+      default:
+        if (resultText === '0') {
+          showResult(value);
+        } else {
+          resultText += value;
+          showResult(resultText)
+        }
+        break;
+    }
   }
 }
 
 function calculateResult() {
   const currentTemp = parseFloat(temp);
   const currentResult = parseFloat(document.getElementById('result').value);
-
-  if (currentOperator === '+') {
-    temp = currentTemp + currentResult;
-  } else if (currentOperator === '-') {
-    temp = currentTemp - currentResult;
-  } else if (currentOperator === '*') {
-    temp = currentTemp * currentResult;
-  } else if (currentOperator === '/') {
-    temp = currentTemp / currentResult;
-  } else if (currentOperator === null) {
-    temp = document.getElementById('result').value;
+  // console.log(currentTemp);
+  // console.log(currentResult);
+  // console.log(typeof currentTemp);
+  // console.log(typeof currentResult);
+  switch (currentOperator) {
+    case '+':
+      temp = currentTemp + currentResult;
+      break;
+    case '-':
+      temp = currentTemp - currentResult;
+      break;
+    case '*':
+      temp = currentTemp * currentResult;
+      break;
+    case '/':
+      temp = currentTemp / currentResult;
+      break;
+    default:
+      temp = document.getElementById('result').value;
+      break;
   }
 
   const result = parseFloat(parseFloat(temp).toFixed(10));
@@ -53,6 +85,9 @@ function calculateResult() {
 }
 
 function appendOperator(op) {
-  calculateResult();
+  if (!operatorAppended) {
+    calculateResult();
+    operatorAppended = true;
+  }
   currentOperator = op;
 }
